014556d305347f2049ddf2c154b1a2b1c16d5247
gtensha <gtensha@users.noreply.github.com>
2017-08-03 14:12:19 +0200

    Added InputHandler class

---
+++ source/InputHandler.d
@@ -0,0 +1,46 @@
+import derelict.sdl2.sdl;/* : SDL_PollEvent,
+						   SDL_Event,
+						   SDL_Keycode,
+						   SDL_KeyboardEvent,
+						   SDL_EventType;*/
+import Engine : Engine;
+
+class InputHandler {
+
+	private Engine parent;
+	private int[] bindings;
+
+	this(int eventAmount, Engine parent) {
+		this.bindings = new int[eventAmount];
+		this.parent = parent;
+	}
+
+	public int listenKeyboard() {
+		SDL_Event event;
+		while (SDL_PollEvent(&event) == 1) {
+			if (event.type == SDL_KEYDOWN) {
+				foreach (int i, int binding ; bindings) {
+					if (binding == event.key.keysym.sym) {
+						return i;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+
+	public void bind(int event, int key) {
+		if (event < bindings.length && event >= 0) {
+			bindings[event] = key;
+		}
+	}
+
+	public void bind(int[] keys) {
+		if (keys.length <= bindings.length) {
+			foreach (int event, int key ; keys) {
+				bindings[event] = key;
+			}
+		}
+	}
+
+}
