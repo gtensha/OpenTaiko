97aa3df3afe60057cfd19d0fe2104bce48656be0
gtensha <me@gtensha.tokyo>
2018-02-27 14:29:31 +0100

    Remove bounds checking and update documentation in Scene

--- source/Maware/Renderer/Renderables/Scene.d
+++ source/Maware/Renderer/Renderables/Scene.d
@@ -4,23 +4,27 @@ import maware.renderable.renderable;
 
 import std.conv : to;
 
-// A renderable class that holds other renderables to act as a "scene" in the game
+/// A renderable class that holds other renderables to act as a "scene" in the game.
+/// Some of the methods in this class will throw a RangeError if bad indices
+/// are supplied.
 class Scene : Renderable {
 
-	private string name; // the scene's name
+	private string name;
 
-	// The renderables for the renderer to render each render() call.
-	// Uses a layered principle, objects in the lower layers will render first
-	// and at the bottom of the screen
+	/* The renderables for the renderer to render each render() call.
+	   Uses a layered principle, objects in the lower layers will render first
+	   and at the bottom of the screen
+	*/
 	private Renderable[][] renderables;
 
+	/// Create a new Scene with the given name
 	this(string name) {
 		this.name = name;
 	}
 
-	// Render all the renderables in this scene
+	/// Render all the renderables in this scene
 	public void render() {
-		foreach (Renderable[] renderableObjects ; this.renderables) {
+		foreach (Renderable[] renderableObjects ; renderables) {
 			foreach (Renderable renderable ; renderableObjects) {
 				if (renderable !is null) {
 					renderable.render();
@@ -29,70 +33,41 @@ class Scene : Renderable {
 		}
 	}
 
-	// Adds a layer of renderables and returns its index
+	/// Adds a layer of renderables and returns its index
 	public int addLayer() {
 		renderables ~= null;
-		return to!int(renderables.length - 1);
+		return cast(uint)renderables.length - 1;
 	}
 
+	/// Clears the layer at the given index of renderables
 	public void clearLayer(uint layer) {
-		if (layer < renderables.length) {
-			renderables[layer] = null;
-		} else {
-			throw new Exception("Tried to clear non-existent layer (out of bounds)");
-		}
+		renderables[layer] = null;
 	}
 
-	// Registers a renderable to the specified layer and returns index
-	public int addRenderable(int layer, Renderable renderable) {
-		if (renderables.length - 1 < layer) {
-			return -1;
-		} else {
+	/// Registers a renderable to the specified layer and returns its index
+	public int addRenderable(uint layer, Renderable renderable) {
 			renderables[layer] ~= renderable;
-			return to!int(renderables[layer].length - 1);
-		}
+			return cast(uint)renderables[layer].length - 1;
 	}
 
-	// Remove a renderable from the scene and return it if found,
-	// else returns null
+	/// Remove a renderable from the scene and return it if found
 	public Renderable removeRenderable(int layer, int index) {
-		if (layer < renderables.length && index < renderables[layer].length) {
-			Renderable tempRenderable = renderables[layer][index];
-			renderables[layer][index] = null;
-			return tempRenderable;
-		} else {
-			return null;
-		}
+		Renderable tempRenderable = renderables[layer][index];
+		renderables[layer][index] = null;
+		return tempRenderable;
 	}
 
-	// Returns the object contained at the specified position, or null if
-	// empty/unspecified space
+	/// Returns the object contained at the specified position
 	public Renderable objectAt(int layer, int index) {
-		if (renderables.length > layer) {
-			if (renderables[layer].length > index) {
-				return renderables[layer][index];
-			} else {
-				return null;
-			}
-		} else {
-			return null;
-		}
+		return renderables[layer][index];
 	}
 
-	// Returns the object contained at the specified position and replaces
-	// it with the object from arguments, returns null if invalid index
+	/// Returns the object contained at the specified position and replaces
+	/// it with the object from arguments
 	public Renderable setObjectAt(Renderable renderable, int layer, int index) {
-		if (renderables.length > layer) {
-			if (renderables[layer].length > index) {
-				Renderable toReplace = renderables[layer][index];
-				renderables[layer][index] = renderable;
-				return toReplace;
-			} else {
-				return null;
-			}
-		} else {
-			return null;
-		}
+		Renderable toReplace = renderables[layer][index];
+		renderables[layer][index] = renderable;
+		return toReplace;
 	}
 
 	/// Returns the name of this scene
