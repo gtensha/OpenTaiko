4073f672c3dc931c94f05ba531582e38b4c54681
gtensha <me@gtensha.tokyo>
2018-05-03 01:30:25 +0200

    Fix indentation

--- source/OpenTaiko/Keybinds.d
+++ source/OpenTaiko/Keybinds.d
@@ -5,16 +5,16 @@ import derelict.sdl2.sdl : SDL_GameController;
 /// Structure containing controller and keyboard bindings for a player
 struct Keybinds {
 
-    /// Structure containing keyboard keybinds (SDL_Keycodes) for a player
-    struct Keyboard {
-	    int[4] drumKeys; /// Array of LK->LD->RD->RK drum keybinds	    
-    } 
+	/// Structure containing keyboard keybinds (SDL_Keycodes) for a player
+	struct Keyboard {
+		int[4] drumKeys; /// Array of LK->LD->RD->RK drum keybinds	    
+	} 
 	Keyboard keyboard; /// The keyboard
     
 	/// Structure containing controller keybinds for a player
-    struct Controller {
+	struct Controller {
 		SDL_GameController* activeController; /// The active controller or null for inactive
-	    int[4] drumKeys; /// Array of LK->LD->RD->RK drum keybinds 
+		int[4] drumKeys; /// Array of LK->LD->RD->RK drum keybinds 
 	} 
 	Controller controller; /// The controller
 
