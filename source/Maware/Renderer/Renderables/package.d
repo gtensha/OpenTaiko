962f6c046dfe3587919783a3fd3a143477e8be83
gtensha <me@gtensha.tokyo>
2019-05-30 00:59:53 +0200

    Add missing ellipsedtext package entry

--- source/Maware/Renderer/Renderables/package.d
+++ source/Maware/Renderer/Renderables/package.d
@@ -1,12 +1,13 @@
 module maware.renderable;
 
+public import maware.renderable.boundedtext;
 public import maware.renderable.coloringtextured;
+public import maware.renderable.container;
+public import maware.renderable.compactingtext;
+public import maware.renderable.ellipsedtext;
 public import maware.renderable.menus;
 public import maware.renderable.renderable;
-public import maware.renderable.solid;
-public import maware.renderable.textured;
-public import maware.renderable.text;
-public import maware.renderable.boundedtext;
-public import maware.renderable.compactingtext;
 public import maware.renderable.scene;
-public import maware.renderable.container;
+public import maware.renderable.solid;
+public import maware.renderable.text;
+public import maware.renderable.textured;
